f15f5a8263f5854539fc300b4678a6972385cf43
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-04-08 09:12:22 -0700

    Add Configuration Sample File

---
+++ appfee-web-service/configuration-sample.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * AppFee web service sample configuration file
+ *
+ * Defines constants used by the end user to configure the application for their
+ * environment. This file should be populated with the appropriate values and
+ * saved under the file name configuration.php
+ *
+ * @copyright 2015 Bellevue College
+ * @license GNU General Public License, version 2
+ * @link https://github.com/BellevueCollege/appfee
+ * @package AppFee-WebService
+ * @subpackage Configuration
+ * @since 1.0.0
+ */
+
+/**
+ * Defines the base URI where the application is being hosted.
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'BASE_URI', '' );
+
+/**
+ * Defines the data source name (database connection parameters).
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'DATABASE_DSN', '' );
+
+/**
+ * Defines the user name to use when connecting to the data source.
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'DATABASE_USER', '' );
+
+/**
+ * Defines the password to use when connecting to the data source.
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'DATABASE_PASSWORD', '' );
+
+/**
+ * Defines the shared secret that will be used in the application URL.
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'SHARED_SECRET', '' );
+
+/**
+ * Defines the local timezone.
+ *
+ * @since 1.0.0
+ * @var string
+ */
+define( 'TIMEZONE', '' );
